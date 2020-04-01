87829e764b3dbbd4cc6614ddf38d2194161d2024
Opi Sen Gupta <opisengupta@gmail.com>
2020-04-02 00:43:50 +0600

    Create Slider.php

---
+++ Android-App/EBook/Slider.php
@@ -0,0 +1,20 @@
+[
+    {
+        "id": "1",
+        "title": "Slide 1",
+        "description": "First of three slide",
+        "image_url": "https://cdn.pixabay.com/photo/2015/02/24/15/41/dog-647528__340.jpg"
+    },
+    {
+        "id": "2",
+        "title": "Slide 2",
+        "description": "Second of three slide",
+        "image_url": "https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg"
+    },
+    {
+        "id": "3",
+        "title": "Slide 3",
+        "description": "Third of three slide",
+        "image_url": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/2bb0b6e2-4974-4daa-be42-aae08230b15e/d694cdl-72533059-b58e-4ceb-8e2a-5e76db86becb.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzJiYjBiNmUyLTQ5NzQtNGRhYS1iZTQyLWFhZTA4MjMwYjE1ZVwvZDY5NGNkbC03MjUzMzA1OS1iNThlLTRjZWItOGUyYS01ZTc2ZGI4NmJlY2IuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.0sx-dfvDxs3QT4LVbjcf-YO6wXzQY1l_rzt_H_o4ef0"
+    }
+]
